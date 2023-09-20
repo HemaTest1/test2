c826cdece26f83322046a26058f531be89eabe5f
HemaTest1 <khema.cnp@gmail.com>
2023-09-20 09:59:39 +0530

    a

---
+++ A.java
@@ -0,0 +1,11 @@
+class A 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("i am into test2 repo");
+		System.out.println("i am into test2repo");
+		System.out.println("i am into test2 repo");
+
+		
+	}
+}
